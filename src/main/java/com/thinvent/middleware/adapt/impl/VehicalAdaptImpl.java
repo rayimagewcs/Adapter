12b49ee01f50279e439ee0d971188471ad8f24b7
rayimagewcs <kwuchangsong@163.com>
2017-08-24 15:39:18 +0800

    Create VehicalAdaptImpl.java

---
+++ src/main/java/com/thinvent/middleware/adapt/impl/VehicalAdaptImpl.java
@@ -0,0 +1,15 @@
+package com.thinvent.middleware.adapt.impl;
+
+import com.thinvent.library.exception.ThinventBaseException;
+import com.thinvent.middleware.adapt.IVehicalAdapt;
+import com.thinvent.middleware.model.ThinventTemplate;
+
+public class VehicalAdaptImpl implements IVehicalAdapt {
+
+	@Override
+	public String registerVehicalTemplate(ThinventTemplate template) throws ThinventBaseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
