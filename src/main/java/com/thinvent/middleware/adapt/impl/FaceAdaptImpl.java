96727595c62e3fff00d12ca6754a677405afff74
rayimagewcs <kwuchangsong@163.com>
2017-08-24 15:38:50 +0800

    Create FaceAdaptImpl.java

---
+++ src/main/java/com/thinvent/middleware/adapt/impl/FaceAdaptImpl.java
@@ -0,0 +1,15 @@
+package com.thinvent.middleware.adapt.impl;
+
+import com.thinvent.library.exception.ThinventBaseException;
+import com.thinvent.middleware.adapt.IFaceAdapt;
+import com.thinvent.middleware.model.ThinventTemplate;
+
+public class FaceAdaptImpl implements IFaceAdapt {
+
+	@Override
+	public String registerFaceTemplate(ThinventTemplate template) throws ThinventBaseException {
+		// TODO webservice or socket invoke
+		return null;
+	}
+
+}
